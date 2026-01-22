bedabe3563065ca645322f626fab160c22a98b79
Gabriel Speranceta <gabriel.hesperanceta@gmail.com>
2026-01-21 21:02:46 -0300

    feat: add display for muscle power and total energy indicators in evaluation details

--- src/app/(tests)/30sts/[id]/page.tsx
+++ src/app/(tests)/30sts/[id]/page.tsx
@@ -135,6 +135,7 @@ export default function Page() {
           <Typography variant="h6" sx={{ mb: 2 }}>
             Informações da Avaliação
           </Typography>
+
           <Grid container spacing={2}>
             <Grid size={4}>
               <InfoItem label="Tipo" value={"30SSTS"} />
@@ -159,6 +160,41 @@ export default function Page() {
           </Grid>
         </CardContent>
       </Card>
+
+      <Grid container spacing={2} sx={{ mb: 2 }}>
+        <Grid size={6}>
+          {extraEvaluations.derived.indicators
+            .filter((ind) => ind.name === "Power")
+            .map((ind, idx) => (
+              <Card variant="outlined" key={idx}>
+                <CardContent>
+                  <Typography variant="body1" sx={{ mb: 1 }}>
+                    Potência Muscular
+                  </Typography>
+                  <Typography variant="body1" sx={{ fontWeight: "bold" }}>
+                    {ind.value.toFixed(2)} mW
+                  </Typography>
+                </CardContent>
+              </Card>
+            ))}
+        </Grid>
+        <Grid size={6}>
+          {extraEvaluations.derived.indicators
+            .filter((ind) => ind.name === "Total Energy")
+            .map((ind, idx) => (
+              <Card variant="outlined" key={idx}>
+                <CardContent>
+                  <Typography variant="body1" sx={{ mb: 1 }}>
+                    Energia Total
+                  </Typography>
+                  <Typography variant="body1" sx={{ fontWeight: "bold" }}>
+                    {ind.value.toFixed(2)} mJ
+                  </Typography>
+                </CardContent>
+              </Card>
+            ))}
+        </Grid>
+      </Grid>
 
       {/* Análise Funcional */}
       {indicadoresRadar && (
