02a8c072f3c30f2d728e611556fa7e98d40c6a07
Gabriel Speranceta <gabriel.hesperanceta@gmail.com>
2026-02-23 20:18:18 -0300

    fix: update patientId references in history repetitions fetching and adjust data structure

--- src/features/evaluations/features/30sts/hooks/useFetchHistoryRepetitions.ts
+++ src/features/evaluations/features/30sts/hooks/useFetchHistoryRepetitions.ts
@@ -2,17 +2,18 @@ import { evaluationService } from "@/features/evaluations/services/evaluation.se
 import { useQuery } from "@tanstack/react-query";
 
 export function useFetchHistoryRepetitions({
-  evaluationId,
+  patientId,
 }: {
-  evaluationId: string;
+  patientId: string;
 }) {
   return useQuery({
-    queryKey: ["30sts", "historyRepetitions", evaluationId],
+    queryKey: ["30sts", "historyRepetitions", patientId],
     queryFn: async () => {
-      const resp = await evaluationService.getRepetitionsHistory(evaluationId);
-      return resp;
+      const resp = await evaluationService.getRepetitionsHistory(patientId);
+      console.log("API Response for history repetitions:", resp);
+      return resp.data;
     },
-    enabled: !!evaluationId,
+    enabled: !!patientId,
     staleTime: 1000 * 60 * 10,
   });
 }
